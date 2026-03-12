3e259d780d9079e548ffad8798c18230cc94ea93
Ujjwal Sahu <ujjwalsahu19@gmail.com>
2026-03-12 15:52:01 +0530

    bugfix: cors issue

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ connectDB();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://the-social-network-olive.vercel.app/'],
+  origin: ['http://localhost:5173', 'https://the-social-network-olive.vercel.app'],
   credentials: true,
 }))
 
